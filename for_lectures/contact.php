06eb168948a46d41e2bf855c507d9bf57f712ea5
Nahhmiehh <crutaborres@gmmail.com>
2025-03-10 21:36:05 +0800

    0215_12_link-from-page-to-page

---
+++ for_lectures/contact.php
@@ -0,0 +1,12 @@
+<?php
+
+$business = [
+    'name' => 'Laracasts',
+    'cost' => 15,
+    'categories' => ["Testing", "PHP", "JavaScript"]
+];
+
+function register($user)
+{
+}
+require "contact.view.php";
