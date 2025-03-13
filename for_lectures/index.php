2d0c8a4151eea4c1e1615adc5095e4948206c8d6
Nahhmiehh <crutaborres@gmmail.com>
2025-03-13 23:23:57 +0800

    0215_17-pdo-first-steps_lec

--- for_lectures/index.php
+++ for_lectures/index.php
@@ -1,14 +1,16 @@
 <?php
 
-// Connect to the MySQL database.
+require 'functions.php';
+
 $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
 $pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("select * from posts where id = 1");
+$statement = $pdo->prepare("select * from posts");
 $statement->execute();
 
 $post = $statement->fetch(PDO::FETCH_ASSOC);
 
-echo "<li>" . $post['title'] . "</li>";
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
