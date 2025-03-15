3ea5951e260bb54b669738e73b46bf130dba29b1
Nahhmiehh <crutaborres@gmmail.com>
2025-03-15 20:13:30 +0800

    0211_3_create-the-obligatory-three-page-layout-using-layout-files_lab

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/about', function () {
     return view('about');
     #return 'About Page';
     #return ['foo' => 'bar'];
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
