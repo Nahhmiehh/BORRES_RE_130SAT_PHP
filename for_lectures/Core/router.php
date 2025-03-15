b9dd8f80713c0ebc39e119f2d401a5b971130f34
Nahhmiehh <crutaborres@gmmail.com>
2025-03-15 23:41:07 +0800

    0315_46-form-validation-exceptions_lec

--- for_lectures/Core/router.php
+++ for_lectures/Core/router.php
@@ -2,43 +2,82 @@
 
 namespace Core;
 
-class Authenticator
+use Core\Middleware\Authenticated;
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
+
+class Router
 {
-    public function attempt($email, $password)
+    protected $routes = [];
+
+    public function add($method, $uri, $controller)
     {
-        $user = App::resolve(Database::class)
-            ->query('select * from users where email = :email', [
-                'email' => $email
-            ])->find();
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method,
+            'middleware' => null
+        ];
 
-        if ($user) {
-            if (password_verify($password, $user['password'])) {
-                $this->login([
-                    'email' => $email
-                ]);
+        return $this;
+    }
 
-                return true;
+    public function get($uri, $controller)
+    {
+        return $this->add('GET', $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        return $this->add('POST', $uri, $controller);
+    }
+
+    public function delete($uri, $controller)
+    {
+        return $this->add('DELETE', $uri, $controller);
+    }
+
+    public function patch($uri, $controller)
+    {
+        return $this->add('PATCH', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        return $this->add('PUT', $uri, $controller);
+    }
+
+    public function only($key)
+    {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+        return $this;
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                Middleware::resolve($route['middleware']);
+
+                return require base_path('Http/controllers/' . $route['controller']);
             }
         }
 
-        return false;
+        $this->abort();
     }
 
-    public function login($user)
+    public function previousUrl()
     {
-        $_SESSION['user'] = [
-            'email' => $user['email']
-        ];
-
-        session_regenerate_id(true);
+        return $_SERVER['HTTP_REFERER'];
     }
 
-    public function logout()
+    protected function abort($code = 404)
     {
-        $_SESSION = [];
-        session_destroy();
+        http_response_code($code);
 
-        $params = session_get_cookie_params();
-        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+        require base_path("views/{$code}.php");
+
+        die();
     }
 }
